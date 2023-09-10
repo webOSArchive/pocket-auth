4e650c58cf68960b7650e56fab14459efc52ee14
codepoet80 <jon.wise@jonandnic.com>
2023-09-10 17:51:31 -0400

    update welcome text

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ include($docRoot . "config.php");
 include($docRoot . "common.php");
 include($docRoot . "inc/header.php");
 ?>
-<p align="middle">Learn more about Pocket for webOS in the app!</p>
+<p align="middle">Pocket is back on webOS! You'll need an app for your device to get started.</p>
+<p align="middle">Check out <a href="<?php echo $REQUEST_PROTOCOL; ?>://appcatalog.webosarchive.org/app/ReadOnTouchPRO3.0">ReadOnTouch Pro</a> in the App Museum!</p>
 <?php
 include($docRoot . "inc/footer.php");
 ?>
