b89edf202d899246e8146cba9bc4c40d1ca830f2
codepoet80 <jon.wise@jonandnic.com>
2023-10-03 10:51:01 -0400

    add activate link

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ echo file_get_contents("https://www.webosarchive.org/app-template/header.php?doc
 ?>
 <p align="middle">Pocket is back on webOS! You'll need an app for your device to get started.</p>
 <p align="middle">Check out <a href="<?php echo findProtocol(); ?>://appcatalog.webosarchive.org/app/ReadOnTouchPRO3.0">ReadOnTouch Pro</a> in the App Museum!</p>
+<br/>
+<p align="middle">Got the app and ready to <a href="/activate">activate</a>?
 <?php
 include($docRoot . "inc/footer.php");
 ?>
